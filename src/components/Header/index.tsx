6035561bb81dd6f27493a0e4bb189020cc5eb86d
Henrique <henriquemkramer@gmail.com>
2022-05-09 20:12:40 -0300

    :tada: Projeto finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink';
+import Link from 'next/link';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Link href="/">
+          <img src="/images/logo.svg" alt="ig.news" />  
+        </Link>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
@@ -16,3 +24,4 @@ export function Header() {
     </header>
   )
 }
+
